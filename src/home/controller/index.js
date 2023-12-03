ddb72cc8c077e91ca16c106060cef9066ab75f42
superman <1689851268@qq.com>
2023-12-03 16:23:54 +0800

    给模板文件赋值

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -2,6 +2,7 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
     indexAction() {
+        this.assign({ title: 'ThinkJS', time: Date.now() }); // 给模板赋值
         return this.display();
     }
 };
